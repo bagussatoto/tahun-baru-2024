ce3b11cf0ae62e7e415664058b2b1cbecb7287ab
bagussatoto <bagusbudisatoto2001@gmail,com>
2023-12-31 10:36:10 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import Particles from "react-particles";
+import { loadFireworksPreset } from "tsparticles-preset-fireworks";
+import { Typewriter } from "react-simple-typewriter";
+import Countdown from "react-countdown";
+
+function App() {
+  const [newYearMessage, setNewYearMessage] = useState(["Bye 2023👋"]);
+
+  const particlesInit = async (preset) => {
+    await loadFireworksPreset(preset);
+  };
+
+  const timeLeft = () => {
+    const newYearDate = new Date("January 1, 2024 00:00:00").getTime();
+    const now = new Date().getTime();
+    const difference = newYearDate - now;
+    return difference;
+  };
+
+  return (
+    <>
+      <Particles init={particlesInit} options={{ preset: "fireworks" }} />
+      <div className="flex justify-center items-center min-h-screen flex-col gap-4">
+        <span className="text-white font-bold text-4xl text-center px-4 z-50">
+          <Typewriter
+            words={newYearMessage}
+            loop={false}
+            cursor
+            cursorStyle={"_"}
+            typeSpeed={70}
+            deleteSpeed={50}
+            delaySpeed={1000}
+          />
+        </span>
+        <div className="text-white font-bold text-2xl z-50 ">
+          <Countdown
+            date={Date.now() + timeLeft()}
+            onComplete={() =>
+              setNewYearMessage([
+                "Selamat",
+                "Tahun",
+                "Baru",
+                "2024",
+                "✨Bagus Budi Satoto✨",
+              ])
+            }
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
